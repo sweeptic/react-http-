0f08e2ee2bd7e4d48224e16512d3071d8640ff3d
Attila <59293104+sweeptic@users.noreply.github.com>
2020-04-30 14:27:02 +0200

    3. Setting Up Links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import axios from "axios";
 import axios from '../../axios'
 
 import Post from "../../components/Post/Post";
@@ -8,66 +7,76 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false,
-    };
+   state = {
+      posts: [],
+      selectedPostId: null,
+      error: false,
+   };
 
-    componentDidMount() {
-        axios
-            .get("/posts")
-            .then((response) => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map((post) => {
-                    return {
-                        ...post,
-                        author: "Max",
-                    };
-                });
-                this.setState({
-                    posts: updatedPosts,
-                });
-                console.log(response);
-            })
-            .catch((error) => {
-                // console.log(error)
-                this.setState({ error: true });
+   componentDidMount() {
+      axios
+         .get("/posts")
+         .then((response) => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map((post) => {
+               return {
+                  ...post,
+                  author: "Max",
+               };
             });
-    }
+            this.setState({
+               posts: updatedPosts,
+            });
+            console.log(response);
+         })
+         .catch((error) => {
+            // console.log(error)
+            this.setState({ error: true });
+         });
+   }
 
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    };
+   postSelectedHandler = (id) => {
+      this.setState({ selectedPostId: id });
+   };
 
-    render() {
-        let posts = (
-            <p style={{ textAlign: "center" }}>Something went wrong!</p>
-        );
+   render() {
+      let posts = (
+         <p style={{ textAlign: "center" }}>Something went wrong!</p>
+      );
 
-        if (!this.state.error) {
-            posts = this.state.posts.map((post) => (
-                <Post
-                    clicked={() => this.postSelectedHandler(post.id)}
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                />
-            ));
-        }
+      if (!this.state.error) {
+         posts = this.state.posts.map((post) => (
+            <Post
+               clicked={() => this.postSelectedHandler(post.id)}
+               key={post.id}
+               title={post.title}
+               author={post.author} />
+         ));
+      }
 
-        return (
-            <div>
-                <section className="Posts">{posts}</section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+      return (
+         <div className="Blog">
+
+            <header>
+               <nav>
+                  <ul>
+                     <li><a href="/">Home</a></li>
+                     <li><a href="/new-post">New Post</a></li>
+                  </ul>
+               </nav>
+            </header>
+
+
+            <section className="Posts">{posts}</section>
+            <section>
+               <FullPost id={this.state.selectedPostId} />
+            </section>
+            <section>
+               <NewPost />
+            </section>
+         </div>
+      );
+   }
 }
 
 export default Blog;
