725071aae6abb1c751327016c654cc064fdceb8d
Attila <59293104+sweeptic@users.noreply.github.com>
2020-05-03 12:50:48 +0200

    24. Redirecting Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Blog.module.css";
 import Posts from './Posts/Posts';
-import { Route, NavLink, Switch } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
@@ -33,6 +33,7 @@ class Blog extends Component {
             <Switch>
                <Route path="/new-post" component={NewPost} />
                <Route path="/posts" component={Posts} />
+               <Redirect from="/" to="/posts" />
             </Switch>
          </div>
       );
--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -18,7 +18,8 @@ class FullPost extends Component {
 
     loadData() {
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
+            if (!this.state.loadedPost || 
+                (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
                 axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         // console.log(response);
